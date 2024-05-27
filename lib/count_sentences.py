1fc649d363c600a3a0dfb1c10d9b7439ee4cffd7
Victor Leina <mtgrt1369@gmail.com>
2024-05-27 23:28:03 +0300

    tried

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, str):
+            self._value = val
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        import re
+        # This regex will match sentences ending with ., ?, or !
+        sentences = re.findall(r'\b[^.!?]*[.!?]', self.value)
+        return len(sentences)
+
